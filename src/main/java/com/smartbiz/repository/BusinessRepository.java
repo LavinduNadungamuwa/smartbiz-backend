ae56e2ea2b0c02dc0d86b5229630293c07001760
LavinduNadungamuwa <lavindunadungamuwa@gmail.com>
2026-04-16 17:32:48 +0530

    Add JPA repositories

---
+++ src/main/java/com/smartbiz/repository/BusinessRepository.java
@@ -0,0 +1,10 @@
+package com.smartbiz.repository;
+
+import com.smartbiz.entity.Business;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BusinessRepository extends JpaRepository<Business, Long> {
+    Optional<Business> findByEmail(String email);
+}
