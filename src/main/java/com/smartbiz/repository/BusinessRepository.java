8c669424c6bf09fcacb8244bc9e36c7026b6b0b6
LavinduNadungamuwa <lavindunadungamuwa@gmail.com>
2026-05-29 17:16:07 +0530

    login and register finalized

--- src/main/java/com/smartbiz/repository/BusinessRepository.java
+++ src/main/java/com/smartbiz/repository/BusinessRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface BusinessRepository extends JpaRepository<Business, Long> {
-    Optional<Business> findByEmail(String email);
+//    Optional<Business> findByEmail(String email);
 }
--- src/main/java/com/smartbiz/repository/UserRepository.java
+++ src/main/java/com/smartbiz/repository/UserRepository.java
@@ -7,6 +7,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
-    List<User> findByBusinessId(Long businessId);
+
+    boolean existsByEmail(String email);
+
+    List<User> findByBusiness_Id(Long businessId);
 }
